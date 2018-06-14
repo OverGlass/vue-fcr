2dae1c6647b7652147cf22deb0b6f1bae3c2bb64
OverGlass <antonin.carlin@gmail.com>
2018-06-14 19:55:06 +0200

    prepare for pre release

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueFcr from './components/VueFcr'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(VueFcr)
 }).$mount('#app')
